f6f53d0088eb4ae7d21f8273e043f0e7f0cf8f4f
Pashtet <dexyo1337@yandex.ru>
2026-07-09 22:56:55 +0300

    Простое число (оператор прерывания break) [#505214]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        boolean isPrime = true;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+}
