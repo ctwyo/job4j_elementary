7226159912f89eeb3adc258f1ce5d8100b057132
Pashtet <dexyo1337@yandex.ru>
2026-07-11 23:49:35 +0300

    6.6.0. Двумерный массив [#505241]

---
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 1;
+        array[0][1] = 1;
+        array[0][2] = 1;
+
+        array[1][3] = 1;
+        array[1][4] = 1;
+        array[1][5] = 1;
+
+        array[2][6] = 1;
+        array[2][7] = 1;
+        array[2][8] = 1;
+    }
+}
