7e67c31166c5d8b102bda66f3c852f8d5d010e14
dexyo1337 <dexyo1337@yandex.ru>
2026-07-17 09:34:36 +0300

    3. Перегрузить метод max для трех чисел [#122644 #565755]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,11 +1,16 @@
 package ru.job4j.condition;
 
 public class Max {
-     public int sum(int a, int b, int c) {
-         return a + b + c;
+
+    public int maxNumber(int a, int b) {
+         return  a > b ? a : b;
     }
 
-    public int sum(int a, int b, int c, int d) {
-        return a + b + c + d;
+    public int maxNumber(int a, int b, int c) {
+        return maxNumber(maxNumber(a, b), c);
+    }
+
+    public int maxNumber(int a, int b, int c, int d) {
+        return maxNumber(maxNumber(a, b, c), d);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,18 +6,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MaxTest {
 
     @Test
-    public void when2Plus2Plus2Then6() {
+    public void when2And2And3Then3() {
         Max max = new Max();
-        int expected = 6;
-        int result = max.sum(2, 2, 2);
+        int expected = 3;
+        int result = max.maxNumber(2, 2, 3);
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void when2Plus2Plus4Plus2Then10() {
+    public void when2And5And4And3Then5() {
         Max max = new Max();
-        int expected = 10;
-        int result = max.sum(2, 2, 4, 2);
+        int expected = 5;
+        int result = max.maxNumber(2, 5, 4, 3);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void when2And5And4And5Then5() {
+        Max max = new Max();
+        int expected = 5;
+        int result = max.maxNumber(2, 5, 4, 5);
         assertThat(result).isEqualTo(expected);
     }
 }
